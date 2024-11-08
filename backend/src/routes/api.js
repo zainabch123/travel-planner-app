481c1e0b1dc1ba97ec75e9baeaf552bbc6eda8a2
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-11-08 12:32:43 +0000

    Debugging api request issues during deployment

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 import { getTravelData } from "../controllers/api.js";
 
-//Get request to tripAdvisorAPI:
+//Get search request from tripAdvisorAPI:
 router.get("/search", getTravelData);
 
 export default router;
